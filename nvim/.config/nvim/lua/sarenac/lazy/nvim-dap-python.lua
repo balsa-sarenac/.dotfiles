05f128189df10c4ad95fb33be95539fb2054b3b9
Balša Šarenac <sarenac.balsa@proton.me>
2024-04-18 19:54:17 +0200

    nvim: add dap for python and dap-ui

---
+++ nvim/.config/nvim/lua/sarenac/lazy/nvim-dap-python.lua
@@ -0,0 +1,11 @@
+return {
+    'mfussenegger/nvim-dap-python',
+    config = function ()
+        local dap_python = require("dap-python")
+        dap_python.setup('~/oll/platform/.venv/bin/python')
+
+        vim.keymap.set("n", "<leader>dn", function() dap_python.test_method() end)
+        vim.keymap.set("n", "<leader>df", function() dap_python.test_class() end)
+        vim.keymap.set("v", "<leader>ds", function() dap_python.debug_selection() end)
+    end
+}
---
+++ nvim/.config/nvim/lua/sarenac/lazy/nvim-dap-ui.lua
@@ -0,0 +1,16 @@
+return {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+        "mfussenegger/nvim-dap",
+        "nvim-neotest/nvim-nio",
+        "folke/neodev.nvim",
+    },
+    config = function()
+        require("neodev").setup({
+            library = {
+                plugins = { "nvim-dap-ui" },
+                types = true
+            },
+        })
+    end
+}
