5608fc31ef40409fb1a91c50d06859fbe71745d2
Balša Šarenac <sarenac.balsa@proton.me>
2024-06-06 21:13:49 +0200

    nvim: new config migrations

--- nvim/.config/nvim/lua/sarenac/lazy/colors.lua
+++ nvim/.config/nvim/lua/sarenac/lazy/colors.lua
@@ -1,19 +1,32 @@
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
     config = function()
-        require('rose-pine').setup({
-            disable_background = true,
+        require("modus-themes").setup({
+            variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+            dim_inactive = true,
             styles = {
-                italic = false,
-            },
-            --- @usage 'auto'|'main'|'moon'|'dawn'
-            variant = 'dawn',
-            --- @usage 'main'|'moon'|'dawn'
-            dark_variant = 'main',
+                comments = { italic = false },
+                keywords = { italic = false }
+            }
         })
-
-        vim.cmd.colorscheme("rose-pine")
-
+        vim.cmd.colorscheme("modus") -- modus_operandi, modus_vivendi
     end
+    --    "rose-pine/neovim",
+    --    name = "rose-pine",
+    --    config = function()
+    --        require('rose-pine').setup({
+    --            disable_background = true,
+    --            styles = {
+    --                italic = false,
+    --            },
+    --            --- @usage 'auto'|'main'|'moon'|'dawn'
+    --            variant = 'dawn',
+    --            --- @usage 'main'|'moon'|'dawn'
+    --            dark_variant = 'main',
+    --        })
+    --
+    --        vim.cmd.colorscheme("rose-pine")
+    --
+    --    end
 }
