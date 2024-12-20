ce7e836b64ef00c04c601cbfceb054235987bb29
Balša Šarenac <sarenac.balsa@proton.me>
2024-12-21 00:08:56 +0100

    nvim: term utilities

--- nvim/.config/nvim/lua/sarenac/remap.lua
+++ nvim/.config/nvim/lua/sarenac/remap.lua
@@ -45,4 +45,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end
 })
 
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.keymap.set("n", "<space>st", function ()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+end)
+
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
