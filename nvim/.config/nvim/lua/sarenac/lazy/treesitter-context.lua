1967ac41b1a813002888374a54fd85c2f39d139b
Balša Šarenac <sarenac.balsa@proton.me>
2025-05-11 23:00:55 +0200

    nvim: lower treesitter-context to be more friendly for laptop screen

--- nvim/.config/nvim/lua/sarenac/lazy/treesitter-context.lua
+++ nvim/.config/nvim/lua/sarenac/lazy/treesitter-context.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
+    require('treesitter-context').setup({
+      multiline_threshold = 10,
+    })
     vim.keymap.set("n", "[c", function()
       require("treesitter-context").go_to_context(vim.v.count1)
     end, { silent = true })
